1f4edd27b1eb7c402ea93200d3e29416fe7723f5
daniel komoe <daniel.komoe@epitech.eu>
2024-07-17 10:04:27 +0200

    [ADD] adding @babel/plugin-proposal-private-property-in-object to DevDependencies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import netlifyIdentity from 'netlify-identity-widget';
 
